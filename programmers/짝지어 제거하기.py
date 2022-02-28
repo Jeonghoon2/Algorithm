17de2e0a9b2e0e1cfd1b2092620f0c9089d40299
JeongHun <jeonghun@JeongHuns-MacBook-Pro.local>
2022-02-28 18:09:58 +0900

    짝지어 제거하기 수정

--- programmers/짝지어 제거하기.py
+++ programmers/짝지어 제거하기.py
@@ -2,15 +2,12 @@ def solution(s):
     stack = []
 
     for i in range(len(s)):
-        if stack.__sizeof__ == 0: 
-            stack.append(s[i]) 
-        else:  
-            if stack[-1] == s[i]: 
-                stack.pop() 
-            else:
-                stack.append(s[i])
+        if not stack: # 스택에 비어 있을때 
+            stack.append(s[i]) # 스택에 s[i]번째 단어를 넣는다
+        else: #스택에 비어 있지 않을때
+            if stack[-1] == s[i]: #스택에 들어가있는 단어와 s[i]번째 단어가 같은지 비교한다.
+                stack.pop() #같다면 밖으로 꺼낸다
+            else: #다르다면 
+                stack.append(s[i]) # 스택에 넣는다.
     if stack == 0 : return 1
     else : return 0
-
-print(solution("baabaa"))
-
