e6298445d37a719b985cc90a2c755265854c72d1
JeongHun <jeonghun@JeongHuns-MacBook-Pro.local>
2022-02-28 18:06:32 +0900

    짝지어 제거하기 수정

---
+++ 짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    stack = []
+    for i in range(len(s)):
+        if not stack == 0: 
+            stack.append(s[i]) 
+        else:
+            if stack[-1] == s[i]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+    if stack == 0 : return 1
+    else : return 0
+
+
