e51dea98bec09e8c44f09b76e510b32b773475dd
JeongHun <jeonghun@JeongHuns-MacBook-Pro.local>
2022-03-02 19:56:56 +0900

    Swap Nodes in Pairs

---
+++ programmers/Swap Nodes in Pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(next=head)
+        pt = dummy.next
+        prev_node = dummy
+        
+        while pt and pt.next:    
+            current_node = pt
+            next_node = current_node.next
+            
+            current_node.next = next_node.next
+            next_node.next = current_node
+            prev_node.next = next_node
+            prev_node = current_node
+            
+            pt = current_node.next
+            
+        return dummy.next
