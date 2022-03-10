94d90f5dcc00036ff3d5071c191f8508a1b8fa19
JeongHun <ukidd12@naver.com>
2022-03-10 14:26:56 +0900

    전화번호 목록 효율성으로 인해 다른 풀이로 작성

--- programmers/전화번호 목록.py
+++ programmers/전화번호 목록.py
@@ -1,11 +1,8 @@
 def solution(phone_book):
+    #오름 차순
+    phone_book.sort()
     #선택 전화번호
     for i in range(len(phone_book)):
-        # 선택 된 전화 번호의 다음 꺼
-        for j in range(i+1,len(phone_book)):
-            # 접두어인지 확인 하기 위해 파이썬 함수인 stringA.startswith(stringB) 함수 사용
-            if phone_book[i].startswith(phone_book[j]):
-                return False
-            if phone_book[j].startswith(phone_book[i]):
-                return False
+        if phone_book[i+1].startswith(phone_book[i]):
+           return False
     return True
